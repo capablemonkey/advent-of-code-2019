cae7eb5a2bd83acd2368bac6e0890851a29e45ab
Gordon Zheng <gordon@greenhouse.io>
2019-12-05 01:29:52 -0500

    refactor day 5

--- day05.rb
+++ day05.rb
@@ -6,8 +6,6 @@ ints = input_lines[0].split(',').map(&:to_i)
 def parse_instruction(instr)
   padded = instr.to_s.rjust(5, '0')
 
-  opcode = padded[3..-1]
-
   return {
     opcode: padded[3..-1],
     args_immediate: {
@@ -29,65 +27,60 @@ def execute(ints, pos)
   memory = ints.dup
   instr = parse_instruction(ints[pos])
 
-  ap instr
-
   a = memory[pos + 1]
   b = memory[pos + 2]
   c = memory[pos + 3]
 
+  v1 = resolve_argument(a, 1, memory, instr)
+  v2 = resolve_argument(b, 2, memory, instr)
+
   new_pos = pos
 
   if instr[:opcode] == '01'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     puts "#{c} = #{v1} + #{v2}"
     memory[c] = v1 + v2
     new_pos += 4
+
   elsif instr[:opcode] == '02'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     puts "#{c} = #{v1} * #{v2}"
     memory[c] = v1 * v2
     new_pos += 4
+
   elsif instr[:opcode] == '03'
     # input = 1
     input = 5
     puts "memory[#{a}] = #{input}"
     memory[a] = input
-
     new_pos += 2
+
   elsif instr[:opcode] == '04'
-    v1 = resolve_argument(a, 1, memory, instr)
     puts "OUTPUT: #{v1}"
     new_pos += 2
+
   elsif instr[:opcode] == '05'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     if v1 != 0
       new_pos = v2
       puts "JMP #{v2}"
     else
       new_pos += 3
     end
+
   elsif instr[:opcode] == '06'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     if v1 == 0
       puts "JMP #{v2}"
       new_pos = v2
     else
       new_pos += 3
     end
+
   elsif instr[:opcode] == '07'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     memory[c] = v1 < v2 ? 1 : 0
     new_pos += 4
+
   elsif instr[:opcode] == '08'
-    v1 = resolve_argument(a, 1, memory, instr)
-    v2 = resolve_argument(b, 2, memory, instr)
     memory[c] = v1 == v2 ? 1 : 0
     new_pos += 4
+
   else
     raise "dunno what to do with #{instr[:opcode]}"
   end
@@ -113,7 +106,3 @@ def run_until_halt(ints)
 end
 
 run_until_halt(ints)
-# run_until_halt(ints, 5)
-# run_until_halt([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-# 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-# 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
