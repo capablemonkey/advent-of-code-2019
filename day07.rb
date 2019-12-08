da101003a21d35113d2e9e1d95fe24d197830f39
Gordon Zheng <gordon@greenhouse.io>
2019-12-08 15:26:57 -0500

    add day 7 pt 2

--- day07.rb
+++ day07.rb
@@ -16,86 +16,116 @@ def parse_instruction(instr)
   }
 end
 
-def run_until_halt(ints)
-  memory = ints.dup
-  pos = 0
-  halted = false
-  input_counter = 0
+class Amp
+  attr_accessor :state
 
-  while halted != true
-    instr = parse_instruction(memory[pos])
+  def initialize(ints)
+    @memory = ints.dup
+    @pos = 0
+    @state = :running
+    @last_output = nil
+  end
 
-    a = memory[pos + 1]
-    b = memory[pos + 2]
-    c = memory[pos + 3]
+  def step
+    raise "still awaiting input" if @state == :awaiting_input
+    raise "halted" if @state == :halted
 
-    v1 = instr[:args_immediate][1] ? a : memory[a]
-    v2 = instr[:args_immediate][2] ? b : memory[b]
+    instr = parse_instruction(@memory[@pos])
+
+    a = @memory[@pos + 1]
+    b = @memory[@pos + 2]
+    c = @memory[@pos + 3]
+
+    v1 = instr[:args_immediate][1] ? a : @memory[a || 0]
+    v2 = instr[:args_immediate][2] ? b : @memory[b || 0]
+
+    @state = :running
 
     if instr[:opcode] == '01'
-      memory[c] = v1 + v2
-      pos += 4
+      @memory[c] = v1 + v2
+      @pos += 4
 
     elsif instr[:opcode] == '02'
-      memory[c] = v1 * v2
-      pos += 4
+      @memory[c] = v1 * v2
+      @pos += 4
 
     elsif instr[:opcode] == '03'
-      input = yield(input_counter) # part  2
-      input_counter += 1
-      memory[a] = input
-      pos += 2
+      @state = :awaiting_input
+      @input_dest = a
 
     elsif instr[:opcode] == '04'
-      return v1
-      pos += 2
+      @pos += 2
+      @state = :output
+      @last_output = v1
 
     elsif instr[:opcode] == '05'
       if v1 != 0
-        pos = v2
+        @pos = v2
       else
-        pos += 3
+        @pos += 3
       end
 
     elsif instr[:opcode] == '06'
       if v1 == 0
-        pos = v2
+        @pos = v2
       else
-        pos += 3
+        @pos += 3
       end
 
     elsif instr[:opcode] == '07'
-      memory[c] = v1 < v2 ? 1 : 0
-      pos += 4
+      @memory[c] = v1 < v2 ? 1 : 0
+      @pos += 4
 
     elsif instr[:opcode] == '08'
-      memory[c] = v1 == v2 ? 1 : 0
-      pos += 4
+      @memory[c] = v1 == v2 ? 1 : 0
+      @pos += 4
 
     elsif instr[:opcode] == '99'
-      halted = true
+      @state = :halted
     else
-      raise "not sure what to do #{opcode}"
+      raise "not sure what to do #{instr[:opcode]}"
     end
-  end
-end
 
-def run_with_params(ints, params)
-  output = run_until_halt(ints) do |i|
-    # puts "asking for input #{i} = #{params[i]}"
-    params[i]
+    {
+      state: @state,
+      output: @last_output
+    }
   end
 
-  output
+  def give_input(input)
+    @memory[@input_dest] = input
+    @pos += 2
+    @state = :running
+  end
+
+  def ack_output
+    raise "not in output state" unless @state == :output
+    @state = :running
+    true
+  end
+
+  def step_until_event
+    result = {state: @state}
+
+    while @state == :running
+      result = step
+    end
+
+    result
+  end
 end
 
 def run_sequence(ints, sequence)
   last_output = 0
 
-  sequence.map do |v|
-    last_output = run_with_params(ints, [v, last_output])
-    # puts "output #{last_output}"
-    last_output
+  sequence.each do |p|
+    a = Amp.new(ints)
+    a.step_until_event
+    a.give_input(p)
+    a.step_until_event
+    a.give_input(last_output)
+    event = a.step_until_event
+    last_output = event[:output]
   end
 
   last_output
@@ -106,11 +136,76 @@ def part1(ints)
     padded = i.to_s.rjust(5, '0').split('').map(&:to_i)
     next unless padded.uniq == padded
     next unless padded.all? {|d| d < 5}
-    ap padded
+    ap padded.join("")
     run_sequence(ints, padded)
   end
 
-  ap results.reject(&:nil?).max
+  ap "max: #{results.reject(&:nil?).max}"
+end
+
+def run_sequence_continuous(ints, sequence)
+  # general algorithm:
+  # 0. Initialize all the amplifiers with the sequence; send first signal (0)
+  # 1. Run current program until state is :awaiting_input, :output, or :halted
+  # 2. If state is awaiting input, call give_input with output from previous amp
+  # 3. If state is output, move on to the next amp
+  # 4. If state is halted, and the current amp is the last amp in the chain (E),
+  #     then return its output. Otherwise, move on to the next amp.
+
+  amps = sequence.map do |v|
+    a = Amp.new(ints)
+    r = a.step_until_event
+
+    if a.state != :awaiting_input
+      raise "not in the right state for initialization"
+    end
+
+    a.give_input(v)
+    a
+  end
+
+  loop_end = false
+  last_output = 0
+  current = amps.first
+  current_idx = 0
+
+  while !loop_end
+    event = current.step_until_event
+
+    if event[:state] == :awaiting_input
+      current.give_input(last_output)
+    elsif event[:state] == :output
+      last_output = event[:output]
+      current.ack_output
+
+      current_idx = (current_idx + 1) % amps.size
+      current = amps[current_idx]
+    elsif event[:state] == :halted
+      return event[:output] if current_idx == amps.size - 1
+
+      current_idx = (current_idx + 1) % amps.size
+      current = amps[current_idx]
+    end
+  end
+
+  last_output
+end
+
+def part2(ints)
+  results = (55555..99999).map do |i|
+    padded = i.to_s.rjust(5, '0').split('').map(&:to_i)
+    next unless padded.uniq == padded
+    next unless padded.all? {|d| d >= 5}
+    ap "trying: #{padded.join("")}"
+
+    run_sequence_continuous(ints, padded)
+  end
+
+  ap "max: #{results.reject(&:nil?).max}"
 end
 
 part1(ints)
+
+# test_ints = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+# run_sequence_continuous(test_ints, [9,8,7,6,5])
+part2(ints)
